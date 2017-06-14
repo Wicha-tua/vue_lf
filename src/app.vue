ee4131a632606268503f40c43453223b545c5227
Wicha-tua <1228490834@qq.com>
2017-06-14 20:45:48 +0800

    '1'

---
+++ src/app.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ele-header :seller="seller"></ele-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  import {mapActions, mapGetters} from 'vuex'
+  import header from './components/header/header.vue'
+
+  export default {
+    computed: mapGetters(['seller']),
+
+    created () {
+      this.$store.dispatch('getSeller')
+    },
+
+    components: {
+      'ele-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/mixins.styl"
+
+  .tab
+    height 40px
+    line-height 40px
+    display flex
+    border-1px(rgba(7,17,27,0.1))
+    .tab-item
+      flex-grow 1
+      text-align center
+      font-size 14px
+      color rgb(77,85,93)
+      a
+        display block
+        &.active
+          color:rgb(240,20,20)
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+﻿import Vue from 'vue'
+import VueResource from 'vue-resource'
+import router from './router'
+import app from './app'
+import store from './store'
+
+// 引入mockjs定义的模拟接口模块
+import './mock/mockServer'
+import './common/stylus/fonts.styl'
+
+// 声明使用vue的扩展插件
+Vue.use(VueResource)
+
+new Vue({
+  el: '#app',
+  store,
+  router,
+  components: { app },
+  template: '<app/>'
+})
